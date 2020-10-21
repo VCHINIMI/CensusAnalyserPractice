47ab2866f24ea18cab4e4a193fca39ef0720a234
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-21 13:08:00 +0530

    [Vinay]Refactored: OpenCSV Builder implements ICSVBuilder Interface

--- CensusAnalyser/src/main/java/censusanalyser/OpenCSVBuilder.java
+++ CensusAnalyser/src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -5,16 +5,16 @@ import java.util.Iterator;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-public class OpenCSVBuilder {
-	<E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {		
-		try {
-            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-            csvToBeanBuilder.withType(csvClass);
-            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
-        } catch (IllegalStateException e) {
-            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
-        }		
+public class OpenCSVBuilder implements ICSVBuilder{
+	   public  <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+	        try {
+	            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+	            csvToBeanBuilder.withType(csvClass);
+	            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+	            return csvToBean.iterator();
+	        } catch (IllegalStateException e) {
+	            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+	        }
+	    }
 	}
-}
